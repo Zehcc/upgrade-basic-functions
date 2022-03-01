36c01eb6172baeb62ad3347de5154390b3bd22de
Tomás Sánchez <Tomassanchezmedina92@gmail.com>
2022-03-01 19:49:20 +0100

    chorus: Itineration#5 complete

---
+++ Itineration#5.js
@@ -0,0 +1,30 @@
+/* Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, 
+en caso que existan los elimina para retornar un array sin los elementos duplicados.
+Puedes usar este array para probar tu función: */
+
+let duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+
+  function removeDuplicates(array) {
+      for (let i = 0; i < array.length; i++) {
+          for (let j = i+1; j < array.length; j++){
+              if (array[i] === array[j]){
+                  array.splice(j,1)
+              }    
+          }
+      } return array;
+  }
+
+  console.log(removeDuplicates(duplicates));
+  
