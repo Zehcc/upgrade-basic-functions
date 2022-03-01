31c1b9032295fbc00cdfb16664fc0a950de6895d
Tomás Sánchez <Tomassanchezmedina92@gmail.com>
2022-03-01 19:18:18 +0100

    chore: Itineration #4 complete

--- Itineration#4.js
+++ Itineration#4.js
@@ -1,6 +1,7 @@
 /* Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función: */
 
 let numbers = [12, 21, 38, 5, 45, 37, 6];
+
 function average(array) {
 
     let totalSum = 0;
